77b8dafc26ea306ea187ad33a51cfab99f81f74b
Sean Escriva <sean.escriva@gmail.com>
2013-10-18 16:10:12 -0700

    Initial import of starter files

---
+++ Thorfile
@@ -0,0 +1,7 @@
+require 'emeril/thor'
+require 'emeril/thor_tasks'
+
+Emeril::ThorTasks.new do |t|
+  t.config[:logger].level = :info
+  t.config[:category] = "Other"
+end
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'gluster'
+maintainer 'Heavy Water Ops, LLC'
+maintainer_email 'support@hw-ops.com'
+license 'Apache 2.0'
+description 'Setups up the GlusterFS'
+
+version '0.1.0'
+
